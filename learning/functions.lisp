762aae7cdc4353e705c27a443009284552957bb6
Hiten Tandon <tandonhiten@gamil.com>
2025-01-10 18:04:09 +0530

    Feat: Added Binary Search

--- learning/functions.lisp
+++ learning/functions.lisp
@@ -17,6 +17,20 @@
 
 (defun is-armstrong-number? (x)
   "Checks if the given number is armstrong number"
-  (defun count-digits(x) (if (eq x 0) 0 (1+ (count-digits (truncate x 10)))))
+  (defun count-digits (x) (if (eq x 0) 0 (1+ (count-digits (truncate x 10)))))
   (defun helper (x count) (if (eq x 0) 0 (+ (expt (mod x 10) count) (helper (truncate x 10) count))))
   (eq (helper x (count-digits x)) x))
+
+(defun binary-search (vec x)
+  "This function takes a vec and a num, and returns one of
+    1. If the number is found: 'Found, <posistion>
+    2. If the number is not found: 'NotFound <position the number can be inserted at>"
+    (defun helper (vec x left right) 
+      (let 
+        ((mid (truncate (/ (+ right left) 2))))
+        (cond 
+          ((eq left right) (values 'NotFound left))
+          ((eq x (aref vec mid)) (values 'Found mid))
+          ((< x (aref vec mid)) (helper vec x left mid)) 
+          (t (helper vec x (+ 1 mid) right)))))
+    (helper vec x 0 (length vec)))
